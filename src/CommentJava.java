b3ab165e23857e18f31978346ca5c9f69ab3cc61
DonGoBi <jk961207@gmail.com>
2024-05-17 00:21:11 +0900

    주석

---
+++ src/CommentJava.java
@@ -0,0 +1,16 @@
+public class CommentJava {
+
+	public static void main(String[] args) {
+		System.out.println("hello java1"); //hello java1을 출력합니다
+//		System.out.println("hello java2"); //
+// hello java1을 출력합니다
+
+		/*
+		* 여러줄 주석
+		*
+		*
+		*
+		* */
+	}
+
+}
