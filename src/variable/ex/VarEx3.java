4588f22bf946f9be39a3991ae93a2c35b27110a0
DonGoBi <jk961207@gmail.com>
2024-05-19 02:09:03 +0900

    변수 문제와 풀이

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,14 @@
+package variable.ex;
+
+public class VarEx3 {
+
+	public static void main(String[] args) {
+
+		long num1 = 10000000000L;
+		System.out.println(num1);
+
+		boolean T = true;
+		System.out.println(T);
+	}
+
+}
