7769378175da4950e4b14861be1ff2ed0d1a5d39
DonGoBi <jk961207@gmail.com>
2024-05-19 01:54:42 +0900

    변수 타입2

---
+++ src/variable/Var8.java
@@ -0,0 +1,21 @@
+package variable;
+
+public class Var8 {
+
+	public static void main(String[] args) {
+		//정수
+		byte b = 127; // -128 ~ 127
+		short s = 32767; // -32768 ~ 32767
+		int i = 2137483647; // -21억 ~ 21억 (약 20억)	정수 리터럴
+
+		long l = 9223372036854775807L;	//뒤에 L을 붙여야함
+
+		//실수
+		float f = 10.0f;	// 권장하지 않음
+		double d = 10.0;	// 실수 리터럴
+
+
+
+	}
+
+}
